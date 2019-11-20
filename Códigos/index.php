b2da8c6dd4eca9e45a80fdf423a2671030562a81
João Victor Veronezi <55355294+JoaoVictorVeronezi@users.noreply.github.com>
2019-11-20 17:24:06 -0200

    Outros testes

--- Códigos/index.php
+++ Códigos/index.php
@@ -5,16 +5,21 @@
 <html>
 
 <head>
-    <title>Teste</title>
+    <title>Teste2</title>
 </head>
-
 <body>
-    <h1>Esse formulairo, apenas é um teste das tabelas e da conexão com o banco de dados</h1>
+    <h1>Formulario de cadastro</h1>
     <form action="teste.php" method="POST">
-        Name: <input type="text" name="name"><br>
-        E-mail: <input type="text" name="email"><br>
-        Website: <input type="text" name="website"><br>
-        Comment: <textarea name="comment" rows="5" cols="40"></textarea><br>
+        Nome: <br>
+        <input type="text" placeholder="Entre com seu Nome" name="nome" id="nome"><br>
+        Cargo: <br>
+        <input type="text" placeholder="Entre com seu Cargo" name="cargo" id="cargo"><br>
+        CPF: <br>
+        <input type="text" placeholder="Somente Numeros" name="cpf" id="cpf"><br>
+        Login: <br>
+        <input type="text" placeholder="Entre com seu Login" name="login" id="login"><br>
+        Senha: <br>
+        <input type="password" placeholder="*******" name="senha" id="senha"><br>
         <input type="submit">
     </form>
 
--- Códigos/teste.php
+++ Códigos/teste.php
@@ -1,8 +1,9 @@
 <html>
 <body>
 
-Bem vindo <?php echo $_POST["nome"]; ?><br>
-Seu email é: <?php echo $_POST["email"]; ?>
+Bem vindo <?php echo $_POST["nome"]; ?>, <?php echo $_POST["cargo"]; ?>. <br>
+Seu CPF é: <?php echo $_POST["cpf"]; ?><br>
+Seu login é: <?php echo $_POST["login"]; ?>
 
 </body>
 </html>
