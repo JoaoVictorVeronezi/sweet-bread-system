0140b689f54d3a1fd4b3067068014aa87b9afb89
marcuserp <marcus.cruz@estudante.ufla.br>
2019-12-06 03:28:11 -0200

    Banco SQL

--- Códigos/CodigoTesteCRUDBasico/teste.php
+++
@@ -1,9 +0,0 @@
-<html>
-<body>
-
-Bem vindo <?php echo $_POST["nome"]; ?>, <?php echo $_POST["cargo"]; ?>. <br>
-Seu CPF é: <?php echo $_POST["cpf"]; ?><br>
-Seu login é: <?php echo $_POST["login"]; ?>
-
-</body>
-</html>
